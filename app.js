864ab5f625614185a330d6db3173b396c8800531
jairofilho79 <jairofilho79@gmail.com>
2019-12-04 15:46:20 -0300

    server wip

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+const session = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -10,19 +11,41 @@ var moviesRouter = require('./routes/movies');
 
 var app = express();
 
+const SESSION_SECRET = "ldnnsdlknsdlkvn" // Mudar para uuid
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    name: "id",
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 1*60*1000
+    }
+  })
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/movies', moviesRouter);
+
+app.use((req, res, next) => {
+  if (req.cookies.user_sid && !req.session.user) {
+      res.clearCookie('user_sid');        
+  }
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,7 +60,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,56 @@
 var express = require('express');
+const axios = require('axios');
+const bcrypt = require('bcrypt');
 var router = express.Router();
-
+const jsonServerPort = 1000;
+const saltRounds = 10;
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SDFlix' });
+
+
+
+// middleware function to check for logged-in users
+var sessionChecker = (req, res, next) => {
+  if (req.session.user && req.cookies.user_sid) {
+      console.log(123)
+      res.render('movies')
+  } else {
+      next();
+  }    
+};
+
+router.use(express.json());
+router.use(express.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+// route for Home-Page
+router.get('/', sessionChecker, (req, res) => {
+  console.log(456)
+  res.redirect('/login')
 });
 
 router.get('/login',(req,res,next)=>{
+  console.log(987)
   res.render('login');
+});
+
+router.post('/login', async function(req, res) {
+  const hashPassword = await bcrypt.hash(req.body.password, saltRounds);
+  const user = await axios.get(`http://localhost:${jsonServerPort}/users?email=${req.body.email}&password=${hashPassword}`)
+  
+  if(!user) res.status(401).end("Data incorrect")
+  
+  req.session.user = user.name
+  res.status(200).redirect('/')
+})
+
+router.get('/logout', (req, res) => {
+  if (req.session.user && req.cookies.user_sid) {
+      res.clearCookie('user_sid');
+      res.redirect('/');
+  } else {
+      res.redirect('/login');
+  }
 });
 
 module.exports = router;
