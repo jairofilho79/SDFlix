81a3600c40724972ea3c23cfb0256e1c2c57ca39
jvcanavarro <jvcanavarro@gmail.com>
2019-12-04 14:29:28 -0300

    add json-server to mock db

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var jsonServer = require('json-server');
 var router = express.Router();
 
 /* GET home page. */
@@ -9,5 +10,7 @@ router.get('/', function(req, res, next) {
 router.get('/login',(req,res,next)=>{
   res.render('login');
 });
+
+router.use('/api',jsonServer.router('db.json'));
 
 module.exports = router;
