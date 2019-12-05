e26e1ee393e9495a4c3365fbb7286d446dda0aca
Lucas Souza <lucassouzaufpa@gmail.com>
2019-12-05 05:00:34 -0300

    add user, get users, login
    session pendent

--- routes/index.js
+++ routes/index.js
@@ -1,63 +1,143 @@
-var express = require('express');
-const axios = require('axios');
-const bcrypt = require('bcrypt');
-var jsonServer = require('json-server');
-var router = express.Router();
-const jsonServerPort = 3000;
+const express = require("express");
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const uuid = require("uuid");
+
+const router = express.Router();
 const saltRounds = 10;
-/* GET home page. */
 
+const adapter = new FileSync("db.json");
+const db = low(adapter);
 
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+router.get("/", sessionChecker, (req, res, next) => {
+  res.redirect("/login");
+});
+
+router.get("/login", sessionChecker, (req, res) => {
+  res.render("login");
+});
+
+router.post("/login", loginUser);
+
+router.get("/api/users", listUsers);
+router.post("/api/users", registerUser);
+
+router.get("/api/users", listUsers);
+router.post("/api/users", registerUser);
+
+router.all("*", (req, res) => {
+  res.status(404).send({
+    code: "notFound"
+  });
+});
+
+db.defaults({ users: [], movies: [] }).write();
+
+function listUsers(req, res) {
+  res.send(db.get("users"));
+}
+
+function registerUser(req, res) {
+  let { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).send({
+      code: "missingArgument"
+    });
+    return;
+  }
+
+  const userFind = db
+    .find({ email })
+    .get("users")
+    .value();
+
+  if (userFind) {
+    res.status(400).send({
+      code: "userExist"
+    });
+    return;
+  }
+
+  password = bcrypt.hashSync(password, saltRounds);
+  db.get("users")
+    .push({ email, password })
+    .write();
+
+  res.status(201).send(db.get("users").find({ email }));
+}
+
+function loginUser(req, res){
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).send({
+      code: "missingArgument"
+    });
+    return;
+  }
+
+  const userFind = db
+    .get("users")
+    .find({ email })
+    .value();
+
+  if (!userFind) {
+    res.status(404).send({ code: "userNotFound" });
+    return;
+  }
+
+  if (!bcrypt.compareSync(password, userFind.password)) {
+    res.status(401).send({
+      code: "invalidPassword"
+    });
+    return;
+  }
+  
+  req.session.user = email;
+  res.redirect("/");
+}
 
 // middleware function to check for logged-in users
-var sessionChecker = (req, res, next) => {
+function sessionChecker(req, res, next) {
+  console.log(req.session);
+  console.log(req)
   if (req.session.user && req.cookies.user_sid) {
-      console.log(123)
-      res.render('movies')
+    res.render("movies");
   } else {
-      next();
-  }    
-};
-
-router.use(express.json());
-router.use(express.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
-
-// route for Home-Page
-router.get('/', sessionChecker, (req, res) => {
-  console.log(456)
-  res.redirect('/login')
-});
-
-router.get('/login',(req,res,next)=>{
-  console.log(987)
-  res.render('login');
-});
-
-router.post('/login', async function(req, res) {
- try{if(req.body.password) {}} catch(e) {return}
- const {password, email} = req.body
-  const hashPassword = await bcrypt.hash(password, saltRounds);
-  const axiosResponse = await axios.get(`http://localhost:3000/api/users?email=${req.body.email}`)
-  const user = axiosResponse.data[0]
-
-  if(!user) {res.status(404).end("User not found"); return}
-
-  if(!await bcrypt.compare(password, user.password)) {res.status(401).end("Data incorrect"); return}
-  
-  req.session.user = user.name
-  res.redirect('/')
-})
-
-router.get('/logout', (req, res) => {
-  if (req.session.user && req.cookies.user_sid) {
-      res.clearCookie('user_sid');
-      res.redirect('/');
-  } else {
-      res.redirect('/login');
+    next();
   }
-});
-router.use('/api',jsonServer.router('db.json'));
+}
+
+// router.post('/login', async function(req, res) {
+//  try{if(req.body.password) {}} catch(e) {return}
+
+//  console.log("olha eu aqui")
+//  const {password, email} = req.body
+//   const hashPassword = await bcrypt.hash(password, saltRounds);
+//   const axiosResponse = await axios.get(`http://localhost:3000/api/users?email=${req.body.email}`)
+//   const user = axiosResponse.data[0]
+
+//   if(!user) {res.status(404).end("User not found"); return}
+
+//   if(!await bcrypt.compare(password, user.password)) {res.status(401).end("Data incorrect"); return}
+
+//   req.session.user = user.name
+//   res.redirect('/')
+// })
+
+// router.get('/logout', (req, res) => {
+//   if (req.session.user && req.cookies.user_sid) {
+//       res.clearCookie('user_sid');
+//       res.redirect('/');
+//   } else {
+//       res.redirect('/login');
+//   }
+// });
 
 module.exports = router;
